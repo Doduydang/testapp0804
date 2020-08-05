8bc719c15eaff4efeb41f27bcc4667ebaeec52be
Doduydang <doduydang@gmail.com>
2020-08-05 11:12:21 +0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+
+
+
+//Pages
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup'
+import User from './pages/user'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path ="/" component={Home}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path='/signup' component={Signup}/>
+          <Route exact path="/user" component={User}/>
+        </Switch>
+      </Router>
   );
 }
 
